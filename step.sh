0c1593a490c834cee8468dd4a32ae7bec5d417ad
Viktor Benei <viktor.benei@gmail.com>
2015-09-29 13:08:19 +0200

    minimal bitrise.yml and step.sh revision

--- step.sh
+++ step.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-if [ -z "${path}" ]; then
+if [ -z "${path}" ] ; then
 	echo '[!] Input $path missing!'
 	exit 1
 fi
 
 
-if [ ! -d "${path}" ]; then
-	if [[ "${is_create_path}" == "true" ]]; then
+if [ ! -d "${path}" ] ; then
+	if [[ "${is_create_path}" == "true" ]] ; then
 		mkdir -p "${path}"
 	else
-		echo "${path} doesn't exist, but is_create_path=false"
+		echo "Path (${path}) doesn't exist, but is_create_path=false"
 		exit 1
 	fi
 fi
@@ -20,5 +20,5 @@ fi
 cd "${path}"
 expanded_target_path="$(pwd)"
 
-echo "Change dir to ${expanded_target_path} ..."
+echo "=> Changing working directory to ${expanded_target_path}"
 envman add --key BITRISE_SOURCE_DIR --value "${expanded_target_path}"
